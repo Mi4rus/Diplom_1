076d8f4da54e6c6ae63ad332c151120db16da29d
Mikhail Kolyushin <kolyushin.m@yandex.ru>
2024-11-20 14:15:03 +0500

    Поправлены тесты, теперь они используют моки ингредиента

--- src/test/java/praktikum/BurgerTest.java
+++ src/test/java/praktikum/BurgerTest.java
@@ -1,6 +1,5 @@
 package praktikum;
 
-import org.apache.http.io.SessionOutputBuffer;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -17,10 +16,8 @@ public class BurgerTest {
 
     @Mock
     private Bun bun;
+    @Mock
     private Ingredient ingredient;
-    private Ingredient ingredient1;
-    private Ingredient ingredient2;
-    private Ingredient ingredient3;
 
     @Before
     public void setUp(){
@@ -29,6 +26,7 @@ public class BurgerTest {
     @Test
     public void setBunsTest() {
         Burger burger = new Burger();
+
         burger.setBuns(bun);
 
         assertEquals(bun, burger.bun);
@@ -38,11 +36,10 @@ public class BurgerTest {
     public void addIngredientTest() {
         Burger burger = new Burger();
         burger.setBuns(bun);
-        ingredient = new Ingredient(IngredientType.FILLING,INGREDIENT_NAME, INGREDIENT_PRICE);
 
         assertTrue(burger.ingredients.isEmpty());
-
         burger.addIngredient(ingredient);
+
         assertEquals(1,burger.ingredients.size());
         assertEquals(burger.ingredients.get(0),ingredient);
     }
@@ -50,7 +47,6 @@ public class BurgerTest {
     @Test
     public void removeIngredientTest() {
         Burger burger = new Burger();
-        ingredient = new Ingredient(IngredientType.FILLING,INGREDIENT_NAME, INGREDIENT_PRICE);
         assertTrue(burger.ingredients.isEmpty());
 
         burger.addIngredient(ingredient);
@@ -64,17 +60,16 @@ public class BurgerTest {
     @Test
     public void moveIngredientTest() {
         Burger burger = new Burger();
+        Ingredient ingredient1 = Mockito.mock(Ingredient.class);
+        Ingredient ingredient2 = Mockito.mock(Ingredient.class);
 
-        ingredient1 = new Ingredient(IngredientType.SAUCE,INGREDIENT_NAME, INGREDIENT_PRICE);
-        ingredient2 = new Ingredient(IngredientType.SAUCE,INGREDIENT_NAME, INGREDIENT_PRICE);
-        ingredient3 = new Ingredient(IngredientType.FILLING,INGREDIENT_NAME, INGREDIENT_PRICE);
+        burger.addIngredient(ingredient);
         burger.addIngredient(ingredient1);
         burger.addIngredient(ingredient2);
-        burger.addIngredient(ingredient3);
         burger.moveIngredient(1, 2);
 
-        assertEquals(burger.ingredients.get(1),ingredient3);
-        assertEquals(burger.ingredients.get(2),ingredient2);
+        assertEquals(burger.ingredients.get(1),ingredient2);
+        assertEquals(burger.ingredients.get(2),ingredient1);
     }
 
     @Test
@@ -82,7 +77,7 @@ public class BurgerTest {
         Burger burger = new Burger();
         burger.setBuns(bun);
         Mockito.when(bun.getPrice()).thenReturn(BUN_PRICE);
-        ingredient = new Ingredient(IngredientType.SAUCE,INGREDIENT_NAME, INGREDIENT_PRICE);
+        Mockito.when(ingredient.getPrice()).thenReturn(INGREDIENT_PRICE);
         List<Ingredient> ingredients = new ArrayList<>();
 
         for(int i =0; i<3;i++){
@@ -97,17 +92,17 @@ public class BurgerTest {
     @Test
     public void getReceipt() {
         Burger burger = new Burger();
-        bun = new Bun(BUN_NAME,BUN_PRICE);
+        Mockito.when(bun.getName()).thenReturn(BUN_NAME);
+        Mockito.when(bun.getPrice()).thenReturn(BUN_PRICE);
         burger.setBuns(bun);
-        ingredient = new Ingredient(IngredientType.SAUCE,INGREDIENT_NAME, INGREDIENT_PRICE);
+        Mockito.when(ingredient.getType()).thenReturn(IngredientType.SAUCE);
+        Mockito.when(ingredient.getName()).thenReturn(INGREDIENT_NAME);
+        Mockito.when(ingredient.getPrice()).thenReturn(INGREDIENT_PRICE);
         burger.addIngredient(ingredient);
         String expectedReceipt = String.format(
                 "(==== %s ====)%n= %s %s =%n(==== %s ====)%n%nPrice: %f%n",
                 bun.getName(),ingredient.getType().toString().toLowerCase(), ingredient.getName(), bun.getName(),burger.getPrice()
         );
-        System.out.println(burger.getReceipt());
-        System.out.println(expectedReceipt);
-
         assertEquals(expectedReceipt, burger.getReceipt());
         }
     }
